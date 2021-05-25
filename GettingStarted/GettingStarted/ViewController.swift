0417da363f1131296880fe2f276e5af025d0f480
akiyama <akiyama@akiyamanoMacBook-ea.local>
2021-05-25 16:41:25 +0900

    予習完了

---
+++ GettingStarted/GettingStarted/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GettingStarted
+//
+//  Created by akiyama on 2021/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var names = ["Tammy", "Cole"]
+        names.removeFirst()
+        names.removeFirst()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- IBBasics/IBBasics/ViewController.swift
+++ IBBasics/IBBasics/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        myButton.setTitleColor(.red, for: .normal)
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func ButtonPressed(_ sender: Any) {
+        print("the button was pressed")
+    }
+    
+    
 }
 
