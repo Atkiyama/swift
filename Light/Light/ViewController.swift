7e0d2d083829bebe14bc3f844fcd29877107f935
akiyama <akiyama@akiyamanoMacBook-ea.local>
2021-05-27 17:52:59 +0900

    本日分終了

--- Light/Light/ViewController.swift
+++ Light/Light/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var lightOn = true
+    
+    
+    @IBOutlet var lightButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
-
+  
+    fileprivate func updateUI() {
+        if lightOn {
+            view.backgroundColor = .white
+            //lightButton.setTitle("OFF" ,for: .normal)
+        }else{
+            view.backgroundColor = .black
+            //lightButton.setTitle("ON" ,for: .normal)
+        }
+    }
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn.toggle()
+        updateUI()
+    }
+    
 
 }
 
